163902e5195a126bdb026d0692041e3e02bc3df9
Rahul Bose <schu82@gmail.com>
2019-05-19 21:50:44 +1000

    Added a Login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-#from <Package name> import <Class(es) within that Package>
+# from <Package name> import <Class(es) within that Package>
+# The lowercase "config" is the name of the Python module 
+# config.py, and obviously the one with the uppercase "C" is the actual class.
+
 from flask import Flask
 from config import Config
 
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 #from <Package name> import <Class(es) within that Package>
-from flask_wtf import Flask
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-Class LoginForm(FlaskForm):
+class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
